b83645ceeddd6a6f4f3c2e3edc74ba4b656637ad
OLZHAS0602 <cool.nic21@gmail.com>
2025-05-14 00:24:57 +0600

    .

--- service/API/infrastructure/database/commands/client.py
+++ service/API/infrastructure/database/commands/client.py
@@ -92,7 +92,7 @@ async def add_return_purchases(
         )
     if client:
         user_id = client.id
-    if not await session.get(ClientPurchase, purchase_return_model.purchase_id):
+    if not await session.get(ClientPurchase, purchase_return_model.purchaseId):
         return {
             "statusCode": 404,
             "message": f"Purchase с id {purchase_return_model.purchase_id} не найден базе",
