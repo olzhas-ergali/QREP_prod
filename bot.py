bbd8e9d236b78944508668079c89a1d59e0f4d81
OLZHAS0602 <cool.nic21@gmail.com>
2025-05-22 23:43:07 +0600

    .

--- bot.py
+++ bot.py
@@ -108,20 +108,20 @@ async def main():
         args=(bot, db.pool,),
     )
 
-    # scheduler.add_job(
-    #     tasks.push_client_about_bonus,
-    #     'cron',
-    #     hour=10,
-    #     minute=00,
-    #     args=(db.pool, bot),
-    #
-    # )
     scheduler.add_job(
         tasks.push_client_about_bonus,
-        'interval',
-        minutes=1,
+        'cron',
+        hour=10,
+        minute=00,
         args=(db.pool, bot),
+
     )
+    # scheduler.add_job(
+    #     tasks.push_client_about_bonus,
+    #     'interval',
+    #     minutes=1,
+    #     args=(db.pool, bot),
+    # )
     scheduler.add_job(
         staff_task.push_staff_about_dismissal,
         'cron',
