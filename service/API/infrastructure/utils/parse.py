f7e500e6d0117362c83ed2f35a2ff7c55b5bda28
OLZHAS0602 <cool.nic21@gmail.com>
2025-06-20 00:06:28 +0600

    .

--- service/API/infrastructure/utils/parse.py
+++ service/API/infrastructure/utils/parse.py
@@ -5,6 +5,20 @@ def parse_phone(value: str):
     return value.strip().replace("+", '').replace(' ', '')
 
 
+async def parse_date(date):
+    formats = [
+        "%d.%m.%Y",
+        "%Y-%m-%d"
+    ]
+    for ftm in formats:
+        try:
+            d = datetime.strptime(date, ftm)
+            return d
+        except ValueError:
+            pass
+    return None
+
+
 def is_valid_date(date_str):
     try:
         datetime.strptime(date_str, "%Y-%m-%d")
