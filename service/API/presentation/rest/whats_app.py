4e35292a6026b66277d107fde3c6050aecc367d2
OLZHAS0602 <cool.nic21@gmail.com>
2025-08-15 22:59:23 +0600

    .

--- service/API/presentation/rest/whats_app.py
+++ service/API/presentation/rest/whats_app.py
@@ -175,8 +175,8 @@ async def get_client_purchases(
             for bonus in client_bonuses:
                 logging.info(f"accrued_points: {bonus.accrued_points}")
                 logging.info(f"write_off_points: {bonus.write_off_points}")
-                if bonus.expiration_date.date() <= datetime.now().date():
-                    if datetime.now().date() >= bonus.activation_date.date() or bonus.expiration_date.date() is None:
+                if bonus.expiration_date.date() is None or bonus.expiration_date.date() <= datetime.now().date():
+                    if datetime.now().date() >= bonus.activation_date.date():
                         total_earned += bonus.accrued_points if bonus.accrued_points else 0
                         total_spent += bonus.write_off_points if bonus.write_off_points else 0
                     else:
